cc7f4625c43f9ebb70c6d8c678ba7f6472f3049c
Your Name <you@example.com>
2019-03-20 13:52:13 +0700

    proximity

--- app/src/main/java/com/example/sensorry/MainActivity.java
+++ app/src/main/java/com/example/sensorry/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.sensorry;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
+    private Sensor sensorProximity;
+    private TextView txSensorProximity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +25,48 @@ public class MainActivity extends AppCompatActivity {
         sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> sensorList=sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        StringBuilder sensortext =  new StringBuilder();
+        StringBuilder sensoTtext =  new StringBuilder();
         for(Sensor sensor:sensorList){
-            sensortext.append(sensor.getName())
+            sensoTtext.append(sensor.getName())
                     .append(System.getProperty("line.separator"));
         }
         TextView sensorTextView=(TextView)findViewById(R.id.sensor_list);
-        sensorTextView.setText(sensortext);
+        sensorTextView.setText(sensoTtext);
+
+        txSensorProximity=(TextView)findViewById(R.id.sensor_prox);
+        sensorProximity=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensorProximity==null){
+            Toast.makeText(this,"no Proximity Sensor",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(sensorProximity!=null){
+            sensorManager.registerListener(this,sensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType=sensorEvent.sensor.getType();
+        float value= sensorEvent.values[0];
+        if(sensorType==Sensor.TYPE_PROXIMITY){
+            txSensorProximity.setText("Proximity Sensor :"+value);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
